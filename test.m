17ce0b8fbfdea56cdda2dc801a135d4d22e75e66
LorRic_01 <riccardo.lorigiola@gmail.com>
2023-04-06 16:26:55 +0200

    Add plot functions and better implementation

--- test.m
+++ test.m
@@ -1,16 +1,21 @@
+% Benchmark functions
+% @(x) -20*exp(-0.2*sqrt(0.5*(x(1).^2 + x(2).^2))) - exp(0.5*(cos(2*pi*x(1)) + cos(2*pi*x(2)))) + exp(1) + 20;
+% @(x) sum(x.^4 - 16*x.^2 + 5*x, 2)/2;
+
 %% Initialization
-dim = 2;
-f = @(x) sum(x.^2);
-lb = -100*ones(1, dim);
-ub = 100*ones(1, dim);
+dim = 1;
+f = @(x) sum(x.^4 - 16*x.^2 + 5*x, 2)/2;
+lb = -10*ones(1, dim);
+ub = 10*ones(1, dim);
 n_emp = 100;
 n_onl = 100;
 maxIter = 50;
 hive = [];
 cycle = 100;
+opts = struct('nFig', 1, 'showFig', [true, true, true], 'v', true);
 
 
 %% Run optimization
 [opt, hive, time] = ABC(dim, f, lb, ub, ...
                     n_emp, n_onl, maxIter, hive, ...
-                    cycle);
+                    cycle, opts);
